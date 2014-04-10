c84a18663f65783d1aa0e1cd7fb6eeba1ef03bc8
Wang Xuerui <idontknw.wang@gmail.com>
2014-04-10 18:00:01 +0800

    app: 添加 WSGI stub

---
+++ snsfeed/app/wsgi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Wang Xuerui <idontknw.wang@gmail.com>
+#
+# This file is part of snsfeed.
+#
+# snsfeed is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# snsfeed is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with snsfeed.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import os
+
+from weiyu import init
+
+_PROJECT_PATH = os.path.join(os.path.dirname(__file__), '../..')
+os.chdir(_PROJECT_PATH)
+init.inject_app()
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
